ac227e72c613747e671b8af9362dd49cbd17da5f
th3gre3k <walker.mitchel@gmail.com>
2015-04-24 22:05:54 -0700

    removing old files

--- devsite/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import patterns, include, url
-from django.contrib import admin
-
-urlpatterns = patterns('',
-    url(r'^$', include('testsite.urls')),
-    url(r'^polls/', include('polls.urls')),
-    url(r'^admin/', include(admin.site.urls)),
-)
--- urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import patterns, url
-import re
-from testsite import views
-
-urlpatterns = patterns('',
-    url(r'^$', views.home, name='home'),
-)
